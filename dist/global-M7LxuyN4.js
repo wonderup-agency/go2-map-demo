8362fd15f6088dd9748a9bd2704331eab660ac1d
napu <nahuel.haberkorn@gmail.com>
2025-11-12 19:25:44 -0300

    build

--- dist/global-M7LxuyN4.js
+++ dist/global-M7LxuyN4.js
@@ -1,2 +1,1 @@
 function t(){}export{t as default};
-//# sourceMappingURL=global-M7LxuyN4.js.map
--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,1 @@
-export{m as default}from"./main-CBAfaC2f.js";
-//# sourceMappingURL=main.js.map
+export{m as default}from"./main-H_ALbdeg.js";
